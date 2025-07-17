ed26e8be3963a33712930b8a231c230e1587d98b
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-07-17 09:13:34 -0400

    Atualiza rota e cria nova página de destino
    Modifica a rota padrão para o `LandingController` e sua ação `Index`. Adiciona um novo controlador em `LandingController.cs` que retorna a visualização padrão. Cria o arquivo `Index.cshtml` com layout HTML completo e seção de navegação estilizada. Move e melhora o CSS em `landing.css`, implementando um fundo em gradiente e novos estilos para texto e botões.

--- ReportEFormStimulSoft/Program.cs
+++ ReportEFormStimulSoft/Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id=SimpleList}");
+    pattern: "{controller=Landing}/{action=Index}/{id?}");
 
 app.Run();
